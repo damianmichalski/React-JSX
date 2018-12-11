59a58211b7094bb5b5aababa575c3101c008c863
Damian Michalski <damian-michalski@tlen.pl>
2018-12-11 14:04:31 +0100

    React JSX

---
+++ components/Contact.js
@@ -0,0 +1,43 @@
+// var Contact = React.createClass({
+//     propTypes: {
+//         item: React.PropTypes.object.isRequired,
+//     },
+//
+//     render: function() {
+//         return (
+//             React.createElement('div', {className: 'contactItem'},
+//                 React.createElement('img', {
+//                     className: 'contactImage',
+//                     src: 'http://icons.veryicon.com/png/System/Beautiful%20Flat/anchor.png'
+//                 }),
+//                 React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+//                 React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+//                 React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+//                     this.props.item.email
+//                 )
+//             )
+//         )
+//     },
+// });
+
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+    render: function () {
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/Beautiful%20Flat/anchor.png'} />
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        );
+    }
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+// var app = React.createElement(App);
+// ReactDOM.render(app, document.getElementById('app'));
+
+var app = <App />;
+ReactDOM.render(app, document.getElementById('app'));
